248a61b0e0fbeba7cd5401cad31ce5ba87312b98
esaulvn <94525849+esaulvn@users.noreply.github.com>
2024-03-18 18:42:24 +0300

    Update task_1.py

--- hw_2/task_1.py
+++ hw_2/task_1.py
@@ -1,11 +1,11 @@
-from latex_gen_1_2 import generate_table
+from latex_gen_12 import LatexGenerator
 
 example = [['Machine ID', 'Type', 'Manufacturer', 'Production Year'],
             ['M001', 'CNC Milling Machine', 'Haas Automation', 2020],
             ['M002', 'Injection Molding Machine', 'Arburg GmbH + Co KG', 2018],
             ['M003', 'Laser Cutting Machine', 'Trumpf GmbH', 2021]]
 
-latex_code = generate_table(example)
+latex_code = LatexGenerator.generate_table(example)
 
 with open('task_1.tex', 'w') as file:
     file.write(latex_code)
